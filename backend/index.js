9f46c25eb53a32c760589f00672d2eccfb225928
SABHAWAT RAHUL NAIK <sabhawatrahul82@gmail.com>
2024-08-12 22:05:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://nirmaan-mitra-frontend.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
