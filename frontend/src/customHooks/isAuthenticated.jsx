e757f1ce0826ba7cf85d1addcb9055b212433ed5
Rahul naik <sabhawatrahu82@gmail.com>
2024-08-15 13:31:18 +0530

    check

--- frontend/src/customHooks/isAuthenticated.jsx
+++ frontend/src/customHooks/isAuthenticated.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import config from "../../config";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import config from "../../config";
 
-const useIsAuthenticated = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
+// const useIsAuthenticated = () => {
+//   const [isAuthenticated, setIsAuthenticated] = useState(false);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      try {
-        const response = await axios.get(
-          `${config.apiBaseUrl}/isAuthenticated`,
-          {
-            withCredentials: true,
-          }
-        );
-        setIsAuthenticated(response.data.loggedIn);
-      } catch (error) {
-        setIsAuthenticated(false);
-      } finally {
-        setLoading(false);
-      }
-    };
+// //   useEffect(() => {
+// //     const checkAuthentication = async () => {
+// //       try {
+// //         const response = await axios.get(
+// //           `${config.apiBaseUrl}/isAuthenticated`,
+// //           {
+// //             withCredentials: true,
+// //           }
+// //         );
+// //         setIsAuthenticated(response.data.loggedIn);
+// //       } catch (error) {
+// //         setIsAuthenticated(false);
+// //       } finally {
+// //         setLoading(false);
+// //       }
+// //     };
 
-    checkAuthentication();
-  }, []);
+//     checkAuthentication();
+//   }, []);
 
-  return { isAuthenticated, loading };
-};
+//   return { isAuthenticated, loading };
+// };
 
-export default useIsAuthenticated;
+// export default useIsAuthenticated;
